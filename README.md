52a93d0ac55f2117d5c1d382023261425cc51065
yassine-zhang <57878778@qq.com>
2025-02-19 16:17:29 +0800

    feat: 新增在团队协作中 git 提交规范

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # commit-lint
 
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
 To install dependencies:
 
 ```bash
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,27 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
+export default {
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    // type 类型定义
-    'type-enum': [2, 'always', [
-      "feat", // 新功能 feature
-      "fix", // 修复 bug
-      "docs", // 文档注释
-      "style", // 代码格式(不影响代码运行的变动)
-      "refactor", // 重构(既不增加新功能，也不是修复bug)
-      "perf", // 性能优化
-      "test", // 增加测试
-      "chore", // 构建过程或辅助工具的变动
-      "revert", // 回退
-      "build" // 打包
-    ]],
-    // subject 大小写不做校验
-    // 自动部署的BUILD ROBOT的commit信息大写，以作区别
-    'subject-case': [0]
-  }
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-empty": [0, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+  },
 };
-
